04f02b8202188dcb38b56839003fc13bddd55ca2
John McGowan <mcgowanjohnfitz081@gmail.com>
2022-01-14 14:51:55 -0500

    changed hoverdata & added discrete colors

--- test.js
+++ test.js
@@ -1,5 +1,24 @@
-const cats = ["abe", "boe", "rusty"];
-const dogs = ["jack", "jill"];
+colors = [
+  "#4E79A7",
+  "#A0CBE8",
+  "#F28E2B",
+  "#FFBE7D",
+  "#59A14F",
+  "#8CD17D",
+  "#B6992D",
+  "#F1CE63",
+  "#499894",
+  "#86BCB6",
+  "#E15759",
+  "#FF9D9A",
+  "#79706E",
+  "#BAB0AC",
+  "#D37295",
+  "#FABFD2",
+  "#B07AA1",
+  "#D4A6C8",
+  "#9D7660",
+  "#D7B5A6",
+];
 
-const allPets = [...cats, ...dogs];
-console.log(allPets);
+console.log(colors[9.01]);
